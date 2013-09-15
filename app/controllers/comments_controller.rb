30ffaeeba6ff75c8e4f3aa9c3f8addef24df7226
runup <645112281@qq.com>
2013-09-15 20:55:22 +0800

    ccc

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,15 @@ class CommentsController < ApplicationController
 	        flash[:success] = "评论成功!"
 	    else
 	    	redirect_to article_path(@article)
+	    	flash[:success] = "评论格式错误，请重新填写评论！"
 	    end
 	end
+
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article)
+    end
+
 end
